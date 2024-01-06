ee426dd4ac6510882184e1373fa8e04b6f0464bb
Mayur Jadhav <mj.jadhav13@gmail.com>
2024-01-06 18:03:51 +0530

    Invoke rust APIs using tauri

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react';
 import Image from 'next/image'
 import styles from './page.module.css'
+import { invoke } from '@tauri-apps/api/tauri'
+import { message } from '@tauri-apps/api/dialog';
 
 export default function Home() {
+  const [greeting, setGreeting] = useState('');
+
+  const greet = () => {
+    console.log("invoking command")
+    invoke('greet', { name: 'Next.js' })
+        .then(result => setGreeting(result))
+        .catch(console.error)
+  }
+
+  const handleClick = async () => {
+    await message('Hello from Tauri!');
+    greet();
+  };
+
+  useEffect(() => {
+    //greet()
+  }, [])
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -28,67 +52,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      <h1>Welcome to Tauri!</h1>
+      <div>{greeting}</div>
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <button
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={handleClick}
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Greet
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </button>
       </div>
     </main>
   )
